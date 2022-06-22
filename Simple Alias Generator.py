be5591c3a60a2bab4a36584d82e82f01e0812e22
Harrebow <91289089+Harrebow@users.noreply.github.com>
2022-06-22 15:45:20 +0100

    Update Simple Alias Generator.py

--- Simple Alias Generator.py
+++ Simple Alias Generator.py
@@ -1,31 +1,11 @@
-# step 1
-beatles=[]
-print("Step 1:", beatles)
+for ch in "john.smith@pythoninstitute.org":
+    if ch == "@":
+        break
+    print(ch, end="")
 
-# step 2
-beatles.append("John Lennon")
-beatles.append("Paul McCartney")
-beatles.append("George Harrison")
-print("Step 2:", beatles)
 
-# step 3
-for i in range(3):
-    if i < 1:
-        beatles.append(input("Please add Stu Sutcliffe to the Beatles: "))
+for digit in "0165031806510":
+    if digit == "0":
+        print("x", end="")
         continue
-    elif i > 1:
-        beatles.append(input("Please add Pete Best to the Beatles: "))
-print("Step 3:", beatles)
-
-# step 4
-del beatles[4]
-del beatles[3]
-print("Step 4:", beatles)
-
-# step 5
-beatles.insert(0,"Ringo Starr")
-print("Step 5:", beatles)
-
-
-# testing list legth
-print("The Fab", len(beatles))
+    print(digit, end="")
