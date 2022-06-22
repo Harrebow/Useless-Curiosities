cdef930bb2e1af9002cdcced115d1bc750597585
Harrebow <91289089+Harrebow@users.noreply.github.com>
2022-06-22 15:45:36 +0100

    Update Simple Code Optimisation Tester.py

--- Simple Code Optimisation Tester.py
+++ Simple Code Optimisation Tester.py
@@ -1,11 +1,41 @@
-for ch in "john.smith@pythoninstitute.org":
-    if ch == "@":
-        break
-    print(ch, end="")
+import datetime
+a = []
+
+def test():
+    count = 0
+    for i in range(0, 100001):
+        if i % 2 != 0:
+            count+=1
+            print(i)
+    
+    # count = 0
+    # i = 1
+    # while i < 100001:
+    #     if i % 2 != 0:
+    #         count+=1
+    #         print(i)
+    #     i += 1
+
+    # i=1
+    # while i != 100001:
+    #     print(i)
+    #     i+=2
 
 
-for digit in "0165031806510":
-    if digit == "0":
-        print("x", end="")
-        continue
-    print(digit, end="")
+y = datetime.datetime.now()
+test()
+x = datetime.datetime.now()
+z=x-y
+a.append(z)
+y = datetime.datetime.now()
+test()
+x = datetime.datetime.now()
+z=x-y
+a.append(z)
+y = datetime.datetime.now()
+test()
+x = datetime.datetime.now()
+z=x-y
+a.append(z)
+
+print(a)
