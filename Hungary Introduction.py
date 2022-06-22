0e97f97303c07f63b5422c21cc24805f128f29e9
Harrebow <91289089+Harrebow@users.noreply.github.com>
2022-06-22 15:44:24 +0100

    Update Hungary Introduction.py

--- Hungary Introduction.py
+++ Hungary Introduction.py
@@ -1,20 +1,7 @@
-def is_year_leap(year):
-    if year % 4 != 0:
-        return False
-    elif year % 100 != 0:
-        return True
-    elif year % 400 != 0:
-        return False
+def introduction(first_name, last_name, country):
+    if country == "Hungary":
+        print("Hello, my name is", last_name, first_name)
     else:
-        return True
+        print("Hello, my name is", first_name, last_name)
 
-test_data = [1900, 2000, 2016, 1987]
-test_results = [False, True, True, False]
-for i in range(len(test_data)):
-	yr = test_data[i]
-	print(yr,"->",end="")
-	result = is_year_leap(yr)
-	if result == test_results[i]:
-		print("OK")
-	else:
-		print("Failed")
+introduction(input("please enter your first name: "), input("please enter your last name: "), input("please enter your country: "))
